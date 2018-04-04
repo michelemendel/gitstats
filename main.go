b080a2aa3ec9eef3147163216f04f30381630e7c
Michele Mendel <michelemendel@gmail.com>
2018-04-04 20:12:18 +0200

    Prints out result from git fetch and status.

--- main.go
+++ main.go
@@ -3,13 +3,25 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
+	"strings"
 	"text/tabwriter"
 )
 
 const cfgFilenameDefault = "dirs.cfg"
 
 func main() {
+	cfgs := parseConfig()
+	// printTableOfCfgs(cfgs)
+
+	for _, cfg := range cfgs {
+		presentGitStatus(cfg)
+	}
+}
+
+func parseConfig() Configs {
 	// Get filename from CLI args
 	cfgFilename := flag.String("f", cfgFilenameDefault, "a config file")
 	flag.Parse()
@@ -17,7 +29,7 @@ func main() {
 	// Get cfgs as an array of structs
 	cfgs := Parse(*cfgFilename)
 
-	printTableOfCfgs(cfgs)
+	return cfgs
 }
 
 func printTableOfCfgs(cfgs Configs) {
@@ -26,4 +38,55 @@ func printTableOfCfgs(cfgs Configs) {
 		fmt.Fprintf(w, "%v\t%v\t%v\n", i+1, cfg.Name, cfg.Dir)
 	}
 	w.Flush()
+}
+
+func presentGitStatus(cfg Config) {
+	os.Chdir(cfg.Dir)
+	printMainTitle(cfg.Name)
+	printSubTitle(cfg.Dir)
+	gitfetch()
+	res := gitstatus()
+	fmt.Printf("\n###\n%v\n###\n", res)
+}
+
+func gitstatus() string {
+	return git("status")
+}
+
+func gitfetch() string {
+	return git("fetch")
+}
+
+func git(cmd string) string {
+	cmdOutBytes, err := exec.Command("git", cmd).Output()
+	checkErr(err)
+	cmdOut := string(cmdOutBytes)
+	printSubTitle(cmd)
+	printBody(cmdOut)
+	return cmdOut
+}
+
+func cwd() {
+	cwd, _ := os.Getwd()
+	printBody(cwd)
+}
+
+func printMainTitle(str string) {
+	// fmt.Printf("\n/////////////%v\n", str)
+	fmt.Printf("\n--------------------------------------------------\n%v", str)
+	fmt.Printf("\n--------------------------------------------------\n")
+}
+
+func printSubTitle(str string) {
+	fmt.Printf("---> %v\n", strings.ToUpper(str))
+}
+
+func printBody(str string) {
+	fmt.Printf("%v\n", str)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
